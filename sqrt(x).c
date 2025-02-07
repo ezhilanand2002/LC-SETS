5a8635982717e1845633a15ac127b0e09f128585
ezhilanand2002 <168058360+ezhilanand2002@users.noreply.github.com>
2025-02-07 14:42:20 +0530

    sqrt(x).c

---
+++ sqrt(x).c
@@ -0,0 +1,15 @@
+int mySqrt(int x) 
+{
+    if(x==0||x==1)
+    {
+        return x;
+    }
+    for(long long int i=1;i<=x;i++)
+    {
+        if(i*i>x)
+        {
+            return i-1;
+        }
+    }
+return 0;
+}
