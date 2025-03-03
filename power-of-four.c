33221af4f96291b3b5f724a6586cce40f0904d1f
ezhilanand2002 <168058360+ezhilanand2002@users.noreply.github.com>
2025-03-03 18:53:59 +0530

    power-of-four.c

---
+++ power-of-four.c
@@ -0,0 +1,9 @@
+bool isPowerOfFour(long int n) {
+        if(n<=0)return false;
+        if(n & (n-1)) return false;
+        if(n%3==1)
+        {        
+            return true;
+        }  
+        return false;
+    }
